a957c2556f8b622b222d8fa6eb6412ac6ee314b0
LiridonaBallazhi <liridona.ballazhi@outlook.com>
2021-10-09 02:50:07 +0200

    DetyraMatematikore

---
+++ src/ORA3/hyrje/ne/java/DetyraMatematikore.java
@@ -0,0 +1,32 @@
+package ORA3.hyrje.ne.java;
+
+public class DetyraMatematikore {
+    public static void main(String[] args) {
+        System.out.print("a)");
+        System.out.print(6*((-2+3)*(2-1)));
+        System.out.print(6*1*1);
+        System.out.println(6);
+
+        System.out.print("b)");
+        System.out.print(6*(-2+3)+(2-1));
+        System.out.print((6*1)+1);
+        System.out.print(6+1);
+        System.out.println(7);
+
+        System.out.print("c)");
+        System.out.print(6*-2+3*(2-1));
+        System.out.print(-12+3*1);
+        System.out.print(-12+3);
+        System.out.println(-9);
+
+        System.out.println(6*-2+3*2-1);
+        System.out.println(6*-2+3/2-1);
+        System.out.println(5.3+7/2+0.1);
+        System.out.println((5.3+7)/2.0);
+        System.out.println((5.3+7)/2);
+        System.out.println(5.3+(7/2));
+        System.out.println((1.0+2)+((3%4)/5.0));
+        System.out.println(3*2%4/5.0*2*3);
+
+    }
+}
