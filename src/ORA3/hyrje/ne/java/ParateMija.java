672a8cdd5b4a7f6433508e78f25cb25c158ff306
LiridonaBallazhi <liridona.ballazhi@outlook.com>
2021-10-09 03:38:52 +0200

    Paratë e mija

---
+++ src/ORA3/hyrje/ne/java/ParateMija.java
@@ -0,0 +1,29 @@
+package ORA3.hyrje.ne.java;
+
+import java.util.Scanner;
+
+
+    /*
+Shkruaje këtë varg të urdhërave në Java:
+1. Nje ndryshore parate_e_mija inicializohet me 12
+2. parate_e_mija zvogëlohet për 5
+3. parate_e_mija dyfishohet
+4. parate_e_mija resetohet, rivendoset në 1
+5. Vlera e ndryshores parate_e_mija i dërgohet në një println-mesazh objektit
+ */
+    public class ParateMija {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int parate_e_mija = 12 ;
+            System.out.println(parate_e_mija);
+            parate_e_mija -= 5;
+            System.out.println(parate_e_mija);
+            parate_e_mija *= 2;
+            System.out.println(parate_e_mija);
+            parate_e_mija = 1;
+            System.out.println(parate_e_mija);
+            System.out.println(parate_e_mija);
+
+        }
+    }
+
