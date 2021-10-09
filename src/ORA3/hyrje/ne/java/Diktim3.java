0b0547413949c2872ab93c52c3fb8567004d5939
LiridonaBallazhi <liridona.ballazhi@outlook.com>
2021-10-09 02:14:50 +0200

    Diktim3- Operacionet Aritmetike

---
+++ src/ORA3/hyrje/ne/java/Diktim3.java
@@ -0,0 +1,10 @@
+package ORA3.hyrje.ne.java;
+
+public class Diktim3 {
+    public static void main(String[] args) {
+        System.out.println(1+((2-4)*3)+5);
+        System.out.println(1+((-2)*3)+5);
+        System.out.println(1+(-6)+5);
+        System.out.println(-5+5);
+    }
+}
